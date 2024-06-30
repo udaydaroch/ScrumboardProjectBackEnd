f887b9d977ef42c0a5ab8e390e520f708a7763ef
Uday <Uda12@uclive.ac.nz>
2024-06-30 21:48:32 +1200

    fixedd 2

--- src/config/express.ts
+++ src/config/express.ts
@@ -12,10 +12,15 @@ export default () => {
     app.use(bodyParser.raw({type: 'text/plain'}));
     app.use(bodyParser.raw({type: ['image/*'], limit: '5mb'}));
 
-    app.use('*', (req, res) => {
-        res.status(404).send('Page not found');
+    app.use((req, res, next) => {
+        Logger.info(`Received a ${req.method} request for ${req.url}`);
+        next();
     });
 
+    app.use('*', (req, res) => {
+        Logger.info(`No route handler matched for ${req.url}`);
+        res.status(404).send('Page not found');
+    });
     // ROUTES
     require('../app/routes/base.routes')(app)
     require('../app/routes/user.routes')(app);
