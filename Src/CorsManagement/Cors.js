489083b13d70c0980b71c9720d87c81526b2d519
Uday <Uda12@uclive.ac.nz>
2024-07-01 11:33:45 +1200

    made the backend login for login

--- Src/CorsManagement/Cors.js
+++ Src/CorsManagement/Cors.js
@@ -2,7 +2,13 @@ const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
-    next();
+
+    if (req.method === 'OPTIONS') {
+        // Pre-flight request. Reply successfully:
+        res.status(200).json({});
+    } else {
+        next();
+    }
 };
 
 module.exports = corsMiddleware;
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const userRoutes = require('./Src/Routes/UserRoutes');
 const cors = require("./Src/CorsManagement/Cors")
 const app = express();
 
-app.use(userRoutes);
-app.use(cors);
+app.use(cors); // Use cors middleware first
+app.use(userRoutes); // Then use userRoutes middleware
+
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
     console.log(`Server is running on http://localhost:${port}`)
