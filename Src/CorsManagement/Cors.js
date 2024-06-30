b611ee644b723e86c5e31e96209edac01cf63eda
Uday <Uda12@uclive.ac.nz>
2024-07-01 11:47:51 +1200

    made the backend login for login

--- Src/CorsManagement/Cors.js
+++ Src/CorsManagement/Cors.js
@@ -1,11 +1,11 @@
 const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
 
     if (req.method === 'OPTIONS') {
         // Pre-flight request. Reply successfully:
-        res.status(200).json({});
+        res.status(200).end();
     } else {
         next();
     }
