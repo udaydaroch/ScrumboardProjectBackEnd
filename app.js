0642e6acd0cc95867f0ce8fde8ac2dec6cd7aadb
Uday <Uda12@uclive.ac.nz>
2024-07-01 08:52:16 +1200

    added Controller section for each database table

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
 // app.js
-const postgres = require('postgres');
-require('dotenv').config();
-
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-
-const sql = postgres({
-    host: PGHOST,
-    database: PGDATABASE,
-    username: PGUSER,
-    password: PGPASSWORD,
-    port: 5432,
-    ssl: 'require',
-    connection: {
-        options: `project=${ENDPOINT_ID}`,
-    },
-});
-
+const sql = require('./connection');
 
 async function getUsers() {
     const users = await sql`SELECT * FROM users`;
