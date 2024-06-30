bff7d1aab6c88a9d0d3d439dbfea2e76b333e3c8
Uday <Uda12@uclive.ac.nz>
2024-07-01 09:22:26 +1200

    don't need these

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("./Src/CorsManagement/Cors")
 const app = express();
 
 app.use(userRoutes);
-app.use(cors);
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
     console.log(`Server is running on http://localhost:${port}`)
