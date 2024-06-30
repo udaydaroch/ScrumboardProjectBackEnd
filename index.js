436bfa87b7f007f583b9ce5a6d957726169424d4
Uday <Uda12@uclive.ac.nz>
2024-07-01 09:27:24 +1200

    cors connected

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("./Src/CorsManagement/Cors")
 const app = express();
 
 app.use(userRoutes);
+app.use(cors);
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
     console.log(`Server is running on http://localhost:${port}`)
