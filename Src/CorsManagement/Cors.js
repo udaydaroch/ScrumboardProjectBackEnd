b0a5ae74f0941af00394daf850f17b99ed953d02
Uday <Uda12@uclive.ac.nz>
2024-07-01 12:43:47 +1200

    made the backend login for login

--- Src/CorsManagement/Cors.js
+++ Src/CorsManagement/Cors.js
@@ -1,7 +1,8 @@
 const corsMiddleware = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://scrumboard-project.vercel.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+    next();
 };
 
 module.exports = corsMiddleware;
