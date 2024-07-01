a270bfe1f2550229b721f10e4c19b3a310287f8e
Uday <Uda12@uclive.ac.nz>
2024-07-01 12:31:58 +1200

    made the backend login for login

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const userRoutes = require('./Src/Routes/UserRoutes');
+const cors = require("cors")
 const app = express();
 
-
-
-app.use(userRoutes);
+app.use(cors); // Use cors middleware first
+app.use(userRoutes); // Then use userRoutes middleware
 
 app.get('/', (req, res) => {
     res.send('I am here');
@@ -15,3 +15,10 @@ app.listen(port, () =>
     console.log(`Server is running on http://localhost:${port}`)
 );
 
+
+
+
+// Your routes
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
