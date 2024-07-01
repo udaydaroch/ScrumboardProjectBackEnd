4b79a8894f70c584b1e6da89710e3f418e0090d8
Uday <Uda12@uclive.ac.nz>
2024-07-01 13:02:47 +1200

    made the backend login for login

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const userRoutes = require('./Src/Routes/UserRoutes');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({
+/*app.use(cors({
     origin: 'https://scrumboard-project-cbsb6q3lj-udaydarochs-projects.vercel.app/',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
-}));
+}));*/
 
 app.use(express.json());
 
