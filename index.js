48073ad7c07d47231572bb1e36cc1f9600b7263c
Uday <Uda12@uclive.ac.nz>
2024-07-01 12:03:45 +1200

    made the backend login for login

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const app = express();
 
 
 const corsOptions = {
-    origin: ['https://scrumboard-project-cbsb6q3lj-udaydarochs-projects.vercel.app/',
-        "https://scrumboard-project-git-master-udaydarochs-projects.vercel.app/",
-        "https://scrumboard-project.vercel.app/"],
+    origin: function (origin, callback) {
+        const allowedOrigins = ['https://scrumboard-project-cbsb6q3lj-udaydarochs-projects.vercel.app/',
+            "https://scrumboard-project-git-master-udaydarochs-projects.vercel.app/",
+            "https://scrumboard-project.vercel.app/"];
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
     optionsSuccessStatus: 204
 };
+
+app.use(cors(corsOptions));
 
 
 app.use(cors(corsOptions));
