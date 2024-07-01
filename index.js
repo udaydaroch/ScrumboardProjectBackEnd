21675504ab90c60faa0cef4d37fc985de043f830
Uday <Uda12@uclive.ac.nz>
2024-07-01 12:00:39 +1200

    made the backend login for login

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const userRoutes = require('./Src/Routes/UserRoutes');
-const cors = require("./Src/CorsManagement/Cors")
+const cors = require("cors")
 const app = express();
 
-app.use(cors); // Use cors middleware first
-app.use(userRoutes); // Then use userRoutes middleware
+
+const corsOptions = {
+    origin: ['https://scrumboard-project.vercel.app/',
+        "scrumboard-project-git-master-udaydarochs-projects.vercel.app",
+        "scrumboard-project-cbsb6q3lj-udaydarochs-projects.vercel.app"],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+
+scrumboard-project.vercel.app
+scrumboard-project-git-master-udaydarochs-projects.vercel.app
+scrumboard-project-cbsb6q3lj-udaydarochs-projects.vercel.app
+
+
+
+app.use(cors(corsOptions));
+app.use(userRoutes);
 
 app.get('/', (req, res) => {
     res.send('I am here');
@@ -14,3 +30,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () =>
     console.log(`Server is running on http://localhost:${port}`)
 );
+
