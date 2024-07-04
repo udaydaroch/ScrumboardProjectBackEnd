b961d34dba663d31f29a71b65acf863e4cd585a9
Uday <Uda12@uclive.ac.nz>
2024-07-04 13:41:55 +1200

    fixed it so that the subtasks are correctly sent to the frontend

--- Src/Model/TaskModel.js
+++ Src/Model/TaskModel.js
@@ -2,15 +2,15 @@ const sql = require('../../connection');
 
 
 async function getTaskUser(taskId) {
-    return await sql`SELECT * FROM task_user WHERE task_id = ${taskId}`;
+    return await sql`SELECT * FROM tasks_users WHERE task_id = ${taskId}`;
 }
 
 async function setTaskUser(taskId, userId) {
-    return await sql`INSERT INTO task_user (task_id, user_id) VALUES (${taskId}, ${userId})`;
+    return await sql`INSERT INTO tasks_users (task_id, user_id) VALUES (${taskId}, ${userId})`;
 }
 
 async function removeTaskUser(taskId, userId) {
-    return await sql`DELETE FROM task_user WHERE task_id = ${taskId} AND user_id = ${userId}`;
+    return await sql`DELETE FROM tasks_users WHERE task_id = ${taskId} AND user_id = ${userId}`;
 }
 
 module.exports = {getTaskUser, setTaskUser, removeTaskUser};
