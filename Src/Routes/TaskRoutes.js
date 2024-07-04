bf675317352e1e89ed2e475542cbda5ee3e1c9f7
Uday <Uda12@uclive.ac.nz>
2024-07-04 13:57:08 +1200

    fixed it so that the subtasks are correctly sent to the frontend

--- Src/Routes/TaskRoutes.js
+++ Src/Routes/TaskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const task = require('../Controller/TaskController');
 
 router.get('/getTaskUser/:taskId', task.getTaskUser);
-router.post('setTaskUser/task/:taskId/user/:userId', task.setTaskUser);
+router.post('/setTaskUser/task/:taskId/user/:userId', task.setTaskUser);
 router.post('/removeTaskUser/:taskId', task.removeTaskUser);
 
 module.exports = router;
