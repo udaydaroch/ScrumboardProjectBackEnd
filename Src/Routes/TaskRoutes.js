9cfc294e66e53b28b29e26f8ddb5f6995c4068ba
Uday <Uda12@uclive.ac.nz>
2024-07-09 17:51:11 +1200

    connected all the subtask associated routes to the backend fully now

--- Src/Routes/TaskRoutes.js
+++ Src/Routes/TaskRoutes.js
@@ -8,7 +8,7 @@ router.get('/getTaskUser/:taskId', task.getTaskUser);
 router.post('/setTaskUser/task/:taskId/user/:userId/assigning/:assignedUser', task.setTaskUser);
 router.post('/removeTaskUser/:taskId/removedBy/:userId/assigned/:assignedId', task.removeTaskUser);
 
-route.post("/completeTask/:taskId", task.completeTask);
+router.post("/completeTask/:taskId", task.completeTask);
 router.get('/getTaskReviewer/:taskId', task.getTaskReviewer);
 router.post('/setTaskReviewer/task/:taskId/user/:userId/reviewing/:reviewingUser', task.setTaskReviewer);
 router.post('/removeTaskReviewer/:taskId/removedBy/:userId/reviewing/:reviewId', task.removeTaskReviewer);
